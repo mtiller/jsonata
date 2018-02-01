6f959c33f38361c231e1e7da092fb50796c61d9f
Michael Tiller <michael.tiller@gmail.com>
2018-02-01 13:05:50 -0500

    Various configuration changes
    These are based on my ts-jest-sample repo.  I copied the settings over,
    but now I get maximum stack size exceeded error.  Need more
    investigation.

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,10 @@
-module.export = {
-    "globals": {
-      "ts-jest": {
-        "skipBabel": true
-      }
+module.exports = {
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
     },
-    "coverageReporters": [
-      "json",
-      "lcov",
-      "text"
-    ],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-    "collectCoverage": true,
-    "mapCoverage": true,
-    "testRegex": "/__tests__/.*\\.(ts|tsx)$",
-    "testPathIgnorePatterns": [
-      "/node_modules/",
-      "/lib/"
-    ],
-    "moduleFileExtensions": [
-      "ts",
-      "tsx",
-      "js",
-      "jsx",
-      "json",
-      "node"
-    ]
-  }
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testPathIgnorePatterns: ["/lib/", "/node_modules/"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    collectCoverage: true,
+    mapCoverage: true,
+};
